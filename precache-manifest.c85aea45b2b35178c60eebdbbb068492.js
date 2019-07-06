63130ee608b18a294658b5978cf4270fe70ad3a5
noxxid <noxxid.john@gmail.com>
2019-07-05 21:35:15 -0600

    Updates

---
+++ precache-manifest.c85aea45b2b35178c60eebdbbb068492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9429f38195824f560fa97fc5a52163",
+    "url": "/reactMemoryGame/index.html"
+  },
+  {
+    "revision": "21fe0f95b6e06c12e31e",
+    "url": "/reactMemoryGame/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d3f7004e1652c18dff65",
+    "url": "/reactMemoryGame/static/css/main.cfc0febc.chunk.css"
+  },
+  {
+    "revision": "21fe0f95b6e06c12e31e",
+    "url": "/reactMemoryGame/static/js/2.8f878b82.chunk.js"
+  },
+  {
+    "revision": "d3f7004e1652c18dff65",
+    "url": "/reactMemoryGame/static/js/main.1bffbe06.chunk.js"
+  },
+  {
+    "revision": "354b94e82a2156ae6ab1",
+    "url": "/reactMemoryGame/static/js/runtime~main.f27ade77.js"
+  }
+]);
